854d4437599de9b39b8be91c979620e654b61172
Salah <salaheddine.ghamri@yahoo.com>
2025-08-01 18:14:01 +0200

    add init

---
+++ init.lua
@@ -0,0 +1,116 @@
+-- mod-version:3
+local core = require "core"
+local keymap = require "core.keymap"
+local StatusView = require "core.statusview"
+local ime = require "core.ime"
+local system = require "system"
+local style = require "core.style"
+local DocView = require "core.docview"
+local command = require "core.command"
+
+local M = {}
+
+-- TODO: command line should clear the status bar entirely
+-- TODO: console.log stealing the status bar
+
+M.last_user_input = ""
+M.command_prompt_label = ""
+M.in_command = false
+M.user_input = ""
+M.done_callback = nil
+
+function M.start_command(callback)
+  M.in_command = true
+  M.user_input = ""
+  M.done_callback = callback or nil  -- optional
+end
+
+-- customize prompt
+function M.set_prompt(prompt)
+  M.command_prompt_label = string.format("%s:", prompt) 
+end
+    
+-- start command
+function M.start_command(callback)
+  M.in_command = true
+  M.user_input = ""
+  M.done_callback = callback or nil  -- optional
+end
+
+-- get last user input
+function M.get_last_user_input()
+   return M.last_user_input 
+end
+
+-- execute_command
+function M.execute_or_return_command()
+  M.last_user_input = M.user_input
+  M.user_input = ""
+  M.in_command = false
+
+  -- call if it was provided
+  if M.done_callback then
+    M.done_callback(M.last_user_input)
+    M.done_callback = nil
+  end
+end
+
+-- function to hold user input
+function M.command_string()
+  if M.in_command then
+    return { M.command_prompt_label .. M.user_input }
+  end
+  return {}
+end
+
+-- Add status bar item once
+if not core.status_view:get_item("status:command_line") then
+  core.status_view:add_item({
+    name = "status:command_line",
+    alignment = StatusView.Item.LEFT,
+    get_item = M.command_string,
+    position = 1000, -- after other items
+    tooltip = "command line input",
+    separator = core.status_view.separator2
+  })
+end
+
+local original_on_key_pressed = keymap.on_key_pressed
+
+-- Accepts only A-Z and a-z
+local function is_letter_key(k)
+  return #k == 1 and k:match("%a")
+end
+
+function keymap.on_key_pressed(key, ...)
+  if PLATFORM ~= "Linux" and ime.editing then
+    return false
+  end
+
+  if M.in_command then
+    if key == "return" then
+      M.in_command = false
+      M.execute_or_return_command()
+    elseif key == "escape" then
+      M.in_command = false
+      M.user_input = ""
+    elseif key == "backspace" then
+      M.user_input = M.user_input:sub(1, -2)
+    elseif key == "space" then
+      M.user_input = M.user_input .. " "
+    elseif is_letter_key(key) then
+      M.user_input = M.user_input .. key
+    end
+
+    return true  -- prevents key from reaching the editor
+  end
+
+  return original_on_key_pressed(key, ...)
+end
+
+command.add(DocView, {
+    ["command_line:start_command"] = M.start_command,
+})
+
+return M
+
