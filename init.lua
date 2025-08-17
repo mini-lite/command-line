29b96faa83693266b9c53b6434b1c1ac67d64c74
Salah <salaheddine.ghamri@yahoo.com>
2025-08-17 17:29:32 +0200

    added temp high prio message

--- init.lua
+++ init.lua
@@ -2,13 +2,14 @@
 
 -- In progress: exectute even lite xl commands
 
--- TODO: all show messages uses show_message of command line
+-- TODO: organize messages to show
 -- TODO: cancel is other event is captured like moving up down or switching window
 -- TODO: history of commands controlled by up and down
 -- TODO: add exceptions for items to show in status bar
 -- TODO: check how to handle taking applying suggestions
 -- TODO: clearing status bar shoul accept exceptions, like items we want to show next to it
 
+-- DONE: all show messages uses show_message of command line
 -- DONE: cursor shall stop blinking where arraws
 -- DONE: move caret in command line
 -- DONE: put characters in commad line at position of caret
@@ -37,13 +38,14 @@ local common = require "core.common"
 
 local current_instance = nil
 local current_message = nil
+local temp_message = nil
 local current_message_expiry = nil
 local MESSAGE_DURATION = 1 -- seconds
 local status_bar_item_name = "status:command_line"
 local old_log = nil
 local original_timeout = config.message_timeout
-local original_caret_width = style.caret_width 
-local blink_period = 1.0  
+local original_caret_width = style.caret_width
+local blink_period = 1.0
 
 -- helper to clear status bar messages
 local function clear_messages()
@@ -65,7 +67,7 @@ function CommandLine:new()
     command_prompt_label = "",
     caret_pos = 1,
     done_callback = nil,
-    cancel_callback = nil, 
+    cancel_callback = nil,
     suggest_callback = function(_) return {} end
   }, self)
   current_instance = instance
@@ -83,7 +85,7 @@ function CommandLine:start_command(opts)
   self.in_command = true
   self.user_input = ""
   self.done_callback = opts and opts.submit or nil
-  self.cancel_callback = opts and opts.cancel or nil 
+  self.cancel_callback = opts and opts.cancel or nil
   self.suggest_callback = opts and opts.suggest or function(_) return {} end
   current_instance = self
   clear_messages() -- flush messages
@@ -121,11 +123,15 @@ end
 
 function CommandLine:get_message()
   local now = os.time()
-  if current_message then
-    if current_message_expiry == nil or now <= current_message_expiry then
-      return current_message
-    end
+
+  if temp_message and current_message_expiry and now <= current_message_expiry then
+      return temp_message
   end
+
+  if current_message then
+      return current_message
+  end
+
   return {}
 end
 
@@ -146,7 +152,7 @@ function CommandLine:get_status_string()
       style.dim, suggestion_suffix
     }
   else
-      -- show message 
+    -- show message
     return self.get_message()
   end
   return {}
@@ -157,7 +163,7 @@ local item = core.status_view:get_item(status_bar_item_name)
 
 local function render_caret(x, y)
   local t = (core.frame_start or 0) % blink_period
-  if t > blink_period / 2 then return end  -- blink off phase
+  if t > blink_period / 2 then return end -- blink off phase
 
   local h = style.font:get_height()
   renderer.draw_rect(x, y, 2, h, style.caret)
@@ -171,7 +177,7 @@ core.status_view.draw = function(self)
   if current_instance and current_instance.in_command then
     local prompt = current_instance.command_prompt_label
     local caret_pos = current_instance.caret_pos
-    local input = current_instance.user_input        -- only user input
+    local input = current_instance.user_input -- only user input
 
     local prompt_width = style.font:get_width(prompt or "")
     local input_before_caret = (input or ""):sub(1, caret_pos - 1)
@@ -201,7 +207,6 @@ function core.on_event(type, ...)
       current_instance.user_input = before .. text .. after
       current_instance.caret_pos = current_instance.caret_pos + #text
       return true
-
     elseif type == "keypressed" then
       blink_period = 0
       local key = ...
@@ -210,11 +215,9 @@ function core.on_event(type, ...)
       if key == "return" then
         current_instance:execute_or_return_command()
         return true
-
       elseif key == "escape" then
         current_instance:cancel_command()
         return true
-
       elseif key == "backspace" then
         if current_instance.caret_pos > 1 then
           local before = input:sub(1, current_instance.caret_pos - 2)
@@ -223,34 +226,29 @@ function core.on_event(type, ...)
           current_instance.caret_pos = current_instance.caret_pos - 1
         end
         return true
-
       elseif key == "left" then
         if current_instance.caret_pos > 1 then
           current_instance.caret_pos = current_instance.caret_pos - 1
         end
         return true
-
       elseif key == "right" then
         if current_instance.caret_pos <= #current_instance.user_input then
           current_instance.caret_pos = current_instance.caret_pos + 1
         end
         return true
-
       elseif key == "up" then
         --if current_instance:history_up() then
         --  current_instance.caret_pos = #current_instance.user_input + 1
         --end
         return true
-
       elseif key == "down" then
         --if current_instance:history_down() then
         --  current_instance.caret_pos = #current_instance.user_input + 1
         --end
         return true
       end
-
     elseif type == "keyreleased" then
-        blink_period = 1
+      blink_period = 1
     end
   end
 
@@ -266,7 +264,7 @@ setmetatable(api, {
       ran = true
       core.add_thread(function()
         core.status_view:hide_items()
-        core.status_view:show_items({status_bar_item_name, "status:test"})
+        core.status_view:show_items({ status_bar_item_name, "status:test" })
       end)
     end
     rawset(api, key, value)
@@ -274,20 +272,19 @@ setmetatable(api, {
 })
 
 -- duration: 0 constant message
-function api.show_message(content, duration) 
+function api.show_message(content, timeout)
   clear_messages() -- flush messages
   if type(content) ~= "table" then return end
-  current_message = content
-  local now = os.time()
-  if duration == 0 then
-    current_message_expiry = nil  -- permanent
+  if timeout and timeout > 0 then
+    temp_message = content
+    current_message_expiry = os.time() + timeout
   else
-    current_message_expiry = now + (duration or MESSAGE_DURATION)
+    current_message = content
   end
 end
 
 function api.set_item_name(name)
-    status_bar_item_name = name
+  status_bar_item_name = name
 end
 
 function api.add_status_item()
@@ -309,3 +306,4 @@ function api.new()
 end
 
 return api
+
